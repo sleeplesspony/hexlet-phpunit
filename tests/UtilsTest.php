618d4fdf576f0bfbc4a34d6602e77b474e3eef43
Natalia Kozlova <sleeplesspony@gmail.com>
2025-06-24 15:32:31 +0300

    add tests with long text

--- tests/UtilsTest.php
+++ tests/UtilsTest.php
@@ -5,18 +5,25 @@ namespace Hexlet\Phpunit\Tests;
 use PHPUnit\Framework\TestCase;
 use function Hexlet\Phpunit\Utils\reverseString;
 
-// Класс UtilsTest наследует класс TestCase
-// Имя класса совпадает с именем файла
 class UtilsTest extends TestCase
 {
-    // Метод (функция), определенный внутри класса,
-    // Должен начинаться со слова test
-    // Ключевое слово public нужно, чтобы PHPUnit мог вызвать этот тест снаружи
     public function testReverse(): void
     {
-        // Сначала идет ожидаемое значение (expected)
-        // И только потом актуальное (actual)
         $this->assertEquals('', reverseString(''));
         $this->assertEquals('olleh', reverseString('hello'));
+    }
+
+    public function testReverseLongText(): void
+    {
+        $testText = file_get_contents($this->getFixtureFullPath('test.txt'));
+        $resultText = reverseString($testText);
+        $pathToExpectedText = $this->getFixtureFullPath('expected.txt');
+        $this->assertStringEqualsFile($pathToExpectedText, $resultText);
+    }
+
+    public function getFixtureFullPath($fixtureName)
+    {
+        $parts = [__DIR__, 'fixtures', $fixtureName];
+        return realpath(implode('/', $parts));
     }
 }
